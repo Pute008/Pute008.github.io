48f8193376a87c82b0b70d14163b75b022ffd540
Pute008 <felixwestby@outlook.com>
2024-12-09 15:06:04 +0100

    multiplieren funker
    nå kan vil ting du har kjøpt og multipliere nullstilles når du kjøper et nytt sted, den eneste multiplieren som vil være igjen er den nye du får når du kjøper et nytt sted

--- Prosjekt_nettside_del_2/prosjekt-nr2 popcorn clicker/script.js
+++ Prosjekt_nettside_del_2/prosjekt-nr2 popcorn clicker/script.js
@@ -34,6 +34,8 @@ let salt = 0;
 
 let newPlacePris = 10000;
 
+let newPlace = 0;
+
 
 
 // array av bilder
@@ -49,12 +51,14 @@ let bilder = [
 // multiplier, 
 let popcornMultiplier = 1;
 
+let popcornMultiplier2 = 1;
+
 
 // når du trykker på popkornet
 function popcornKlikk() {
 
     // når du trykker på popcorn-boksen vil antallet popcorn øke med "popcornMultiplier"
-    popcornTotal = popcornTotal + 1*popcornMultiplier;
+    popcornTotal = popcornTotal + 1 * popcornMultiplier * popcornMultiplier2;
 
     // teksten vil vise hvor mye popcorn du har
     document.getElementById("undertekst").innerText = "Popcorn: "+ Math.floor(popcornTotal);
@@ -94,9 +98,12 @@ function buySoya() {
         // 5) teksten som viser hvor mye popcorn du har vil endres
         document.getElementById("undertekst").innerText = "Popcorn: "+ Math.floor(popcornTotal);
         document.getElementById("soyaPris").innerText = "Pris " + Math.floor(popcornSmakSoyaPris);
+        document.getElementById("AntallSoya").innerText = "Antall: " + popcornSmakSoya;
     }
     
 }
+
+
 
 let clickerShop = document.getElementById("salgsmannShop");
 
@@ -113,6 +120,7 @@ function buyClickers() {
         popcornPerSek = popcornPerSek+1
         document.getElementById("undertekst").innerText = "Popcorn: " + Math.floor(popcornTotal);
         document.getElementById("salgsmannPris").innerText = "Pris " + Math.floor(clickerPris);
+        document.getElementById("AntallClickers").innerText = "Antall: " + clickers;
     }
 }
 
@@ -137,10 +145,15 @@ function buySalt() {
 
         document.getElementById("undertekst").innerText = "Popcorn: " + Math.floor(popcornTotal);
         document.getElementById("saltPris").innerText = "Pris " + Math.floor(saltPris)
+        document.getElementById("AntallSalt").innerText = "Antall: " + salt;
     }
 }
 
-setInterval(myTimer,1000);
+
+
+let timer=1000
+
+setInterval(myTimer, timer);
 
 function myTimer() {
     popcornTotal = popcornTotal + popcornPerSek *  popcornMultiplier
@@ -149,28 +162,58 @@ function myTimer() {
 
 
 
-
+// når man skal kjøpe et nytt sted, da vil multipleieren få en boost på 1.3
+// alle de andre upgradene vil også nullstille
 let newPlaceShop = document.getElementById("newPlaceShop");
 
 newPlaceShop.addEventListener("click", buyNewPlace)
 
 function buyNewPlace() {
     if (popcornTotal >= newPlacePris) {
-        
         popcornTotal = popcornTotal - newPlacePris;
-
         newPlacePris = newPlacePris * 2;
 
-        popcornMultiplier = popcornMultiplier * 1.3;
+        popcornMultiplier2 = popcornMultiplier2 * 1.3;
 
         document.getElementById("undertekst").innerText = "Popcorn: " + Math.floor(popcornTotal);
         document.getElementById("newPlacePris").innerText = "Pris " + Math.floor(newPlacePris);
+        document.getElementById("AntallNewPlace").innerText = "Antall: " + newPlace;
+
+
+        document.querySelector(".item2").style.background = "url(" + backgroundBilder[1] + ")";
+
+
+
+        // for å nullstille prisen og antallet som sees i spillet
+        // antallet
+        clickers = 0;
+        // prisen
+        clickerPris = 10;
+        document.getElementById("AntallClickers").innerText = "Antall: " + clickers;
+        document.getElementById("salgsmannPris").innerText = "Pris 10";
+
+        // antallet
+        popcornSmakSoya = 0;
+        // prisen
+        popcornSmakSoyaPris = 20;
+        document.getElementById("AntallSoya").innerText = "Antall: " + popcornSmakSoya;
+        document.getElementById("soyaPris").innerText = "Pris 20";
+
+        salt = 0;
+        saltPris = 50;
+        document.getElementById("AntallSalt").innerText = "Antall: " + salt;
+        document.getElementById("saltPris").innerText = "Pris 50";
+        
+
+        popcornMultiplier = 1;
+        popcornPerSek = 0;
+        document.getElementById("undertekst").innerText = "Popcorn: "+popcornTotal;
     }
 }
 
 
-
 let backgroundBilder = [
     "background1.svg",
-    "background2.svg"
+    "background2.svg",
+    "background3.svg"
 ]

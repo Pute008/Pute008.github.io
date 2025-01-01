c1ccf40ff16edc53001a6c44e3befcc960ff0054
Pute008 <felixwestby@outlook.com>
2025-01-01 21:01:34 +0100

    mer kommentarer på koder
    kommentarer på prosjekt nr 3 (RPCS3 guiden)

--- Prosjekt_nettside_del_2/prosjekt-nr3 RPCS3 guide/script.js
+++ Prosjekt_nettside_del_2/prosjekt-nr3 RPCS3 guide/script.js
@@ -1,19 +1,23 @@
+// funksjonen til "Read more" knappen
 document.getElementById("buttonRead").addEventListener("click", openGuide);
 
-
+// når du trykker på den første knappen for å lese guiden
 function openGuide(event) {
     
+    // denne linjen gjør at denne koden vil kjøre 1 gang
     event.preventDefault();
     
+    // "Read more" knappen vil ikke vises mer
     document.getElementById("buttonRead").style.display = "none";
 
+    // login vil vises
     document.getElementById("boxLogin").style.display = "block";
 
     console.log("This is running!");
 }
 
 
-
+// når du trykker lever inn på login
 document.getElementById("login").addEventListener("submit", givePaper);
 
 function givePaper(event) {
@@ -21,16 +25,21 @@ function givePaper(event) {
     // denne eventen gjør at innlogingsfeltet ikke kommer tilbake
     event.preventDefault();
 
+    // alt du har sent inn blir lagret
     let username = document.querySelector("#Username").value;
     let email = document.querySelector("#email").value;
     let password = document.querySelector("#password").value;
 
+    // lagret på console
     console.log("Username " + username);
     console.log("Email " + email);
     console.log("Password " + password);
 
+    // kommer opp en beskjed som sier "takk for svarene"
     alert("Thanks for the answers " + username);
+    // login-boksen vil ikke vises mer
     document.getElementById("boxLogin").style.display = "none"; /* show eller block */
+    // når du har logget inn vil guiden vises
     document.getElementById("boxText").style.display = "block";
 
 }

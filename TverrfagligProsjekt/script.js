d36dda734cad635d86e4840e46a9c01cae1d0312
Pute008 <felixwestby@outlook.com>
2025-01-08 12:21:32 +0100

    spill til prosjektet
    nå holder jeg på å lage spill til nettsiden

--- TverrfagligProsjekt/script.js
+++ TverrfagligProsjekt/script.js
@@ -1,36 +1,8 @@
-// lage en quiz
-// statistikk
-
-let svar
-
-let bilder = [
-    "bilde1",
-    "bilde2",
-    "bilde3",
-    "bilde4",
-    "bilde5",
-    "bilde6",
-    "bilde7",
-    "bilde8",
-    "bilde9",
-    "bilde10",
-    "bilde11",
-    "bilde12"
-]
-
-// bilder.addEventListener("click", trykk);
-
-// function trykk() {
-    
-// }
-
-
-
 document.getElementById("ElektriskAvfallReadMore").addEventListener("click", readElektriskAvfall);
 
 function readElektriskAvfall(event) {
 
-    console.log("Jeg blir trykket på!")
+    console.log("Jeg blir trykket på!");
 
     event.preventDefault();
 
@@ -55,5 +27,49 @@ function keyCode(event) {
     let x = event.keyCode;
     if (x == 27) {
         document.getElementById("ElektriskAvfallText").style.display = "none";
+    }
+}
+
+// game
+
+// lage en quiz
+// statistikk
+
+let bilder = [
+    "bilde1",
+    "bilde2",
+    "bilde3",
+    "bilde4",
+    "bilde5",
+    "bilde6",
+    "bilde7",
+    "bilde8",
+    "bilde9",
+    "bilde10",
+    "bilde11",
+    "bilde12"
+]
+
+
+let poengskar = 0;
+
+bilder.addEventListener("click", trykkPaBilde);
+function trykkPaBilde() {
+    if (bilder = "bilde1") {
+        poengskar += 1;
+        document.getElementById("svar").innerText = "Riktig!"
+        document.getElementById("pointVisning").innerHTML = "Poeng " + poengskar + "/4";
+    }
+
+    if (bilder = "bilde2") {
+        poengskar += 1;
+    }
+
+    if (bilder = "bilde3") {
+        poengskar += 1;
+    }
+
+    else if (bilder = "bilde4") { /*her tenkte jeg at resten kunne stå og skrive at ingen ting ville skje når du trykker på de andre*/
+        poengskar += 1;
     }
 }

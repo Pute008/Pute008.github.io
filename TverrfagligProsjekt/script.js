3d766743ef9b5f29f49dbb7ebf905477d4c98f25
Pute008 <felixwestby@outlook.com>
2025-01-12 20:26:22 +0100

    graf ferdig
    nå har jeg fått lagt til begge grafene, den ene viser fdorbruket ditt i kilo, og den andre viser omtrent hvor mye av en type gjenstand det er i en mobil
    
    nå skal jeg lage tekstbokser for de andre tekstene

--- TverrfagligProsjekt/script.js
+++ TverrfagligProsjekt/script.js
@@ -1,18 +1,25 @@
 const ctx = document.getElementById('myChart');
 
 
-// når du trykker på les mer knappen vil teksten vises
 
+
+// denne koden blir repeated for alle de andre "les mer" knappene
+
+
+// når du trykker på les mer knappen vil teksten vises
 document.getElementById("ElektriskAvfallReadMore").addEventListener("click", readElektriskAvfall);
 
 function readElektriskAvfall(event) {
 
     console.log("Jeg blir trykket på!");
 
+    // koden vil kjøre en gang slik at teksten ikke blir vist på skjermen for alltid
     event.preventDefault();
 
+    // henter teksten fra html filen, har lagd denne variablen slik at vi ikke trenger å skrive document.getElementById flere ganger
     let elektriskAvfallTekst = document.getElementById("ElektriskAvfallText");
 
+    // teksten midtstilles og vises
     elektriskAvfallTekst.style.display = "block";
     elektriskAvfallTekst.style.justifyContent = "center";
     elektriskAvfallTekst.style.opacity = "100";
@@ -26,6 +33,37 @@ function sluttALes() {
     document.getElementById("ElektriskAvfallText").style.display = "none";
     // sticky???
 }
+
+
+
+
+
+document.getElementById("HvorSkalAvfalletGaaReadMore").addEventListener("click", readHvorSkalAvfalletGaa);
+
+function readHvorSkalAvfalletGaa(event) {
+
+    event.preventDefault();
+
+    let hvorSkalAvfalletGaaText = document.getElementById("HvorSkalAvfalletGaaText");
+
+    // teksten midtstilles og vises
+    hvorSkalAvfalletGaaText.style.display = "block";
+    hvorSkalAvfalletGaaText.style.justifyContent = "center";
+    hvorSkalAvfalletGaaText.style.opacity = "100";
+}
+
+// når du trykker på lukk hvor teksten vises vil teksten og boksen skjules
+document.getElementById("sluttALes").addEventListener("click", sluttALes);
+
+function sluttALes() {
+
+    document.getElementById("ElektriskAvfallText").style.display = "none";
+}
+
+
+
+
+
 
 
 // når du trykker på "escape" så skal det skjule seg
@@ -104,23 +142,20 @@ function sendsvar(event) {
 
 
 
-
-// Det andre eksempelet er eit såkalla line chart, dette er ein type linjediagram
-// Sjå ein nærare forklaring her: https://www.chartjs.org/docs/latest/charts/line.html
-
+// her er et annet diagram
 const ctx2 = document.getElementById('myChart2');
 
 new Chart(ctx2, {
     type: 'doughnut',
     data: {
-      labels: ['Gull', 'Sølv', 'Bronse'],
+      labels: ['Metall', 'Plast', 'Keramikk'],
       datasets: [{
-        label: '% av mineraler i en PC',
-        data: [12, 1, 3,],
+        label: '% av hva en mobil inneholder',
+        data: [40, 40, 20],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(255, 206, 86, 0.6)'
+            'rgb(177, 101, 14, 0.6)',
+            'rgb(255, 247, 135)',
+            'rgba(255, 206, 86, 0.6)'
         ],
         borderWidth: 1
       }]
@@ -133,47 +168,3 @@ new Chart(ctx2, {
       }
     }
   });
-
-
-  // game
-
-// lage en quiz
-// statistikk
-
-// let bilder = [
-//     "bilde1",
-//     "bilde2",
-//     "bilde3",
-//     "bilde4",
-//     "bilde5",
-//     "bilde6",
-//     "bilde7",
-//     "bilde8",
-//     "bilde9",
-//     "bilde10",
-//     "bilde11",
-//     "bilde12"
-// ]
-
-// let poengskar = 0;
-
-// bilder.addEventListener("click", trykkPaBilde);
-// function trykkPaBilde() {
-//     if (bilder = "bilde1") {
-//         poengskar += 1;
-//         document.getElementById("svar").innerText = "Riktig!"
-//         document.getElementById("pointVisning").innerHTML = "Poeng " + poengskar + "/4";
-//     }
-
-//     if (bilder = "bilde2") {
-//         poengskar += 1;
-//     }
-
-//     if (bilder = "bilde3") {
-//         poengskar += 1;
-//     }
-
-//     else if (bilder = "bilde4") { /*her tenkte jeg at resten kunne stå og skrive at ingen ting ville skje når du trykker på de andre*/
-//         poengskar += 1;
-//     }
-// }

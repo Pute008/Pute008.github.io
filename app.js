01561f7b9a6d468a04ccf5cbd4ac755dae62dff6
Pute008 <felixwestby@outlook.com>
2025-03-26 10:51:18 +0100

    flyttet på flere filer+ny database
    flyttet på flere filer, nå har jeg også lagt til en database

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+
+const Database = require("better-sqlite3");
+const db = new Database("user.db");
+
+const bcrypt = require("bcrypt");
+
+app.use(express.static("public"));
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const port = 3000;
+
+
+app.get("/getUser", (req, res) => {
+    const users = db.prepare("SELECT * FROM user").all();
+    res.json(users);
+});
+
+app.post("/newUser", async (req, res) => {
+    // får informasjon fra skjemaet i html-filen, skjemaet må være fylt ut
+    const { personnummer, fornavn, etternavn, epost, telefon, password } = req.body;
+
+    // Hash passordet med bcrypt
+    const saltRounds = 10; // Antall runder med hashing
+    const hashPassord = await bcrypt.hash(password, saltRounds);
+
+    // sier hva den skal legge til i databasen
+    const stmt = db.prepare("INSERT INTO user (personnummer, fornavn, etternavn, epost, telefon, password) VALUES (?, ?, ?, ?, ?, ?)");
+    // legger til personen i databasen
+    const info = stmt.run(personnummer, fornavn, etternavn, epost, telefon, hashPassord);
+
+    // vi får en melding tilbake om at personen er lagt til
+    res.json({ message: "New user made", info });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

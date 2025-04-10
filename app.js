34677d3dfd3b2834d5577bb39665beba9e90a49a
Pute008 <felixwestby@outlook.com>
2025-04-10 13:33:40 +0200

    mye er blitt gjort
    nå kan du logge inn, du kan logge ut og nå holder jeg på med databasen slik at jeg kan legge inn og se på data

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(
 
 // Middleware for å beskytte sider bak innloggings-mur
 function kreverInnlogging(req, res, next) {
+    // hvis vi ikke har en bruker som er i en session vil vi bli omdirigert til login-siden
     if (!req.session.bruker) {
         return res.redirect("/login.html");
     }
@@ -40,17 +41,21 @@ function kreverInnlogging(req, res, next) {
 app.post("/login", async (req, res) => {
     const { epost, password } = req.body;
     const bruker = db.prepare("SELECT * FROM user WHERE epost = ?").get(epost);
+    // hvis vi ikke finner riktig epost vil vi få en feilmelding
     if (!bruker) {
         return res.status(401).json({ message: "Feil email eller passord" });
     }
 
     const passordErGyldig = await bcrypt.compare(password, bruker.password);
+    // hvis vi ikke har riktig passord vil vi får en feilmelding
     if (!passordErGyldig) {
         return res.status(401).json({ message: "Feil email eller passord" });
     }
 
     // Lagre brukerdata i session
     req.session.bruker = { id: bruker.fornavn, etternavn: bruker.etternavn };
+    // når vi er logget inn vil vi bli sendt til en ny side (dashboard) hvor ting er skjult for andre brukere
+    // og vi vil få en melding om at innloggingen var vellykket
     res.json({ message: "Innlogging vellykket", redirect: "/skjult" });
 });
 
@@ -58,10 +63,11 @@ app.post("/login", async (req, res) => {
 
 // Rute for å logge ut
 app.post("/logout", (req, res) => {
-    // slutter besøket på siden
+    // når du logger ut vil sessionen bli slettet og du vil bli sendt til login-siden
     req.session.destroy();
     res.json({ message: "Du er logget ut" });
 });
+
 
 // Eksempel på en beskyttet rute
 app.get("/beskyttet", kreverInnlogging, (req, res) => {
@@ -71,7 +77,7 @@ app.get("/beskyttet", kreverInnlogging, (req, res) => {
 
 
 
-
+// henter alle brukere fra user tabellen i databasen
 app.get("/getUser", (req, res) => {
     const users = db.prepare("SELECT * FROM user").all();
     res.json(users);
@@ -98,21 +104,34 @@ app.post("/newUser", async (req, res) => {
 
 
 
-// Rute for å hente bilane til den innlogga brukaren
-// app.get("/minebiler", kreverInnlogging, (req, res) => {
-//     const personnummer = req.session.bruker.id;
+// Rute for å hente bilene til den innlogga brukaren
+app.get("/mineKort", kreverInnlogging, (req, res) => {
+    const cardID = req.session.bruker.id;
 
-//     const biler = db.prepare("SELECT * FROM bil WHERE personnummer = ?").all(personnummer);
+    const kort = db.prepare("SELECT * FROM samling WHERE cardID = ?").all(cardID);
 
-//     res.json(biler);
-// });
+    res.json(kort);
+});
 
 
 
 // Rute for å vise dashboard.html (kun for innlogga brukarar)
 app.get("/skjult", kreverInnlogging, (req, res) => {
+    // sender oss til website.html som er i hidden-mappen
     res.sendFile(__dirname + "/hidden/website.html");
 });""
+
+// hele denne koden sender deg til en nettside (du må bruke denne hvis du har html sider som er utenfor public mappen)
+app.get("/addCardPage/html", kreverInnlogging, (req, res) => {
+    res.sendFile(__dirname + "/hidden/addCard.html");
+});
+
+// sender deg til samling.html siden (som er i hidden mappen)
+app.get("/collection", kreverInnlogging, (req, res) => {
+    res.sendFile(__dirname + "/hidden/samling.html");
+});
+
+
 
 
 app.listen(port, () => {
